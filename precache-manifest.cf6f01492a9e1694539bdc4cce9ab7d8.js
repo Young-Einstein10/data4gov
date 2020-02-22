5797146b079911d7d0b3a86bf6e57a305327d773
young-einstein10 <aabdulrahmanyusuf@gmail.com>
2020-02-22 09:46:42 +0100

    Updates

---
+++ precache-manifest.cf6f01492a9e1694539bdc4cce9ab7d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b79ff07bdc899f610e1289dc00ce7d",
+    "url": "/data4gov/index.html"
+  },
+  {
+    "revision": "d354e0568b2157556d34",
+    "url": "/data4gov/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "00797294fb4ecf509579",
+    "url": "/data4gov/static/css/main.925338e5.chunk.css"
+  },
+  {
+    "revision": "d354e0568b2157556d34",
+    "url": "/data4gov/static/js/2.da525b63.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/data4gov/static/js/2.da525b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00797294fb4ecf509579",
+    "url": "/data4gov/static/js/main.4007a387.chunk.js"
+  },
+  {
+    "revision": "7a28a7e830e97db2bfe3",
+    "url": "/data4gov/static/js/runtime-main.6fb2ba23.js"
+  }
+]);
